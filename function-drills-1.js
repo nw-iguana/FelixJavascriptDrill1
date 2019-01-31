bdef819a274927dcd2544bc0aac29183ee8501d2
Felix Meyer <phelixm@gmail.com>
2019-01-30 20:10:42 -0600

    sixth step finished

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,7 +1,9 @@
+function getYearOfBirth(age) {
+    return 2016 - age;
+}
 function createGreeting(name, age) {
-    const yearOfBirth = 2016 - age;
-    return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}`;
-
+    const yearOfBirth = getYearOfBirth(age);
+    return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting('Felix', 51);
