0ac3cdc7e42e7d9f019eb6fd065aebd1706f4c09
Felix Meyer <phelixm@gmail.com>
2019-01-30 20:47:41 -0600

    added Beyond function

--- function-drills-2.js
+++ function-drills-2.js
@@ -4,3 +4,21 @@ function jediName(firstName, lastName) {
 
 const result = jediName('Beyonce', 'Knowles');
 console.log(result);
+
+function beyond(num) {
+    if (num === Infinity || -Infinity) {
+        console.log('And beyond');
+    }
+    else if (num > 0) {
+        console.log('To infinity');
+    }
+    else if (num < 0) {
+        console.log('To negative infinity');
+    }
+    else if (num === 0) {
+        console.log('Staying home');
+    }
+    else {
+        console.log('What are you trying to accomplish here');
+    }
+}
