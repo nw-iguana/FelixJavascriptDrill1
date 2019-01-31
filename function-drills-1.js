995d1d8efdab5893884901d7057648b60676133e
Felix Meyer <phelixm@gmail.com>
2019-01-30 20:29:35 -0600

    eight step finished

--- function-drills-1.js
+++ function-drills-1.js
@@ -8,12 +8,15 @@ function createGreeting(name, age) {
     if (!(name && age)) {
         throw new Error("Arguments not valid.");
     }
+    if (! (typeof name === 'string' && typeof age === 'number')) {
+        throw new TypeError();
+    }
     const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 try {
-    const greeting1 = createGreeting();
+    const greeting1 = createGreeting(51, "Felix");
     console.log(greeting1);
 }
 catch(error) {
