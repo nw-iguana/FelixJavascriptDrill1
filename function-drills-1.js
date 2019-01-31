83a0cd9c2272df7d96af2a17913eae7984fc012e
Felix Meyer <phelixm@gmail.com>
2019-01-30 19:58:18 -0600

    second step finished

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,1 +1,6 @@
-console.log('Hi, my name is Felix and I\'m 51 years old.');
+function createGreeting() {
+    return 'Hi, my name is Felix and I\'m 51 years old.';
+}
+
+const greeting1 = createGreeting();
+console.log(greeting1);
